0518581276a7d708900bce29e4e166273c2bfcb7
Muhammed Milas M <milasmohammed@gmail.com>
2022-10-01 02:14:49 +0530

    Edge browser driver code and cucumber java, testng version change

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -8,6 +8,7 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class TestBase {
 
@@ -19,15 +20,21 @@ public class TestBase {
 		Properties prop = new Properties();
 		prop.load(fis);
 		String url = prop.getProperty("QAUrl");
+		String browser_properties = prop.getProperty("browser");
+		String browser_maven = System.getProperty("browser");
 		
+		//result = testcondition ? value1 : value2;  --> Java ternary operator
+		String browser = browser_maven!=null ? browser_maven : browser_properties;
+				
 		if(driver == null)
 		{
-			if(prop.getProperty("browser").equalsIgnoreCase("chrome")) {
+			if(browser.equalsIgnoreCase("chrome")) {
 				System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//test//resources//Chromedriver.exe");
 				driver = new ChromeDriver();
 			}
-			else if(prop.getProperty("browser").equalsIgnoreCase("firefox")) {
-				//firefox browser opening code
+			else if(browser.equalsIgnoreCase("edge")) {
+				System.setProperty("webdriver.edge.driver", System.getProperty("user.dir") + "//src//test//resources//msedgedriver.exe");
+				driver = new EdgeDriver();
 			}
 			else{
 				throw new Exception("invalid browser specified.");
